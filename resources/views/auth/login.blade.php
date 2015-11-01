e3428a0dfc912e4f21f316e1b88008c6f7b2ac2e
Walter Neto <walter@catskillet.com>
2015-11-01 15:53:05 -0300

    ajeitando as views de login e home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,91 @@
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+@extends('layouts.login')
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+@section('styles')
+    <style>
+        .demo-card-wide.mdl-card {
+            width: 100%;
+        }
+        .demo-card-wide > .mdl-card__title {
+            color: #fff;
+            height: 200px;
+            background: url('http://lorempixel.com/512/300') center / cover;
+        }
+        .demo-card-wide > .mdl-card__menu {
+            color: #fff;
+        }
+        .padding-login {
+            width: 400px;
+        }
+        .mdl-textfield__input {
+            width: 100%;
+        }
+        .mdl-textfield {
+            width: 350px;
+        }
+    </style>
+@endsection
+
+@section('content')
+
+    <div class="mdl-grid padding-login">
+        <div class="mdl-cell mdl-cell--12-col">
+
+            <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title">
+                    <h2 class="mdl-card__title-text">Login</h2>
+                </div>
+                <div class="mdl-card__supporting-text">
+
+                    <form action="#">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input mdl-cell--12-col" type="text" id="sample3" />
+                            <label class="mdl-textfield__label mdl-cell--12-col" for="sample3">Email</label>
+                        </div>
+
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                            <input class="mdl-textfield__input" type="password" id="sample4" />
+                            <label class="mdl-textfield__label" for="sample3">Password</label>
+                        </div>
+
+                        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-1">
+                            <input type="checkbox" id="checkbox-1" class="mdl-checkbox__input" checked />
+                            <span class="mdl-checkbox__label">Remember-me</span>
+                        </label>
+                    </form>
+
+                </div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--primary mdl-js-button mdl-js-ripple-effect" style="float:right;">
+                        Login
+                    </a>
+                </div>
+            </div>
+
+
+        {{--<form method="POST" action="/auth/login">--}}
+            {{--{!! csrf_field() !!}--}}
+
+            {{--<div>--}}
+                {{--Email--}}
+                {{--<input type="email" name="email" value="{{ old('email') }}">--}}
+            {{--</div>--}}
+
+            {{--<div>--}}
+                {{--Password--}}
+                {{--<input type="password" name="password" id="password">--}}
+            {{--</div>--}}
+
+            {{--<div>--}}
+                {{--<input type="checkbox" name="remember"> Remember Me--}}
+            {{--</div>--}}
+
+            {{--<div>--}}
+                {{--<button type="submit">Login</button>--}}
+            {{--</div>--}}
+        {{--</form>--}}
+
+
+        </div>
     </div>
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+@endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -2,15 +2,62 @@
 <html>
     <head>
         <title>Login</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="/mld/material.min.css">
+        <style>
+            .img-logo {
+                width: 125px;
+                height: 39px;
+                background: url('/img/logo.png') #FFFFFF;
+            }
+        </style>
+        @yield('styles')
     </head>
     <body>
 
-        <div class="container">
-            @yield('content')
+        <!-- Uses a header that scrolls with the text, rather than staying locked at the top -->
+        <div class="mdl-layout mdl-js-layout">
+            <header class="mdl-layout__header mdl-layout__header--scroll">
+                <div class="mdl-layout__header-row">
+                    <!-- Title -->
+                    <span class="mdl-layout-title img-logo"></span>
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                    <!-- Navigation -->
+                    <nav class="mdl-navigation">
+                        {{--<a class="mdl-navigation__link" href="">Link</a>--}}
+                    </nav>
+
+                    {{--<!-- Right aligned menu below button -->--}}
+                    {{--<button id="demo-menu-lower-right" class="mdl-button mdl-js-button mdl-button--icon">--}}
+                        {{--<i class="material-icons">more_vert</i>--}}
+                    {{--</button>--}}
+                    {{--<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">--}}
+                        {{--<li disabled class="mdl-menu__item">Username</li>--}}
+                        {{--<li class="mdl-menu__item">Action</li>--}}
+                        {{--<li class="mdl-menu__item">Some Action</li>--}}
+                        {{--<li class="mdl-menu__item">Another Action</li>--}}
+                    {{--</ul>--}}
+                </div>
+            </header>
+            {{--<div class="mdl-layout__drawer">--}}
+                {{--<span class="mdl-layout-title">Title</span>--}}
+                {{--<nav class="mdl-navigation">--}}
+                    {{--<a class="mdl-navigation__link" href="">Link</a>--}}
+                    {{--<a class="mdl-navigation__link" href="">Link</a>--}}
+                    {{--<a class="mdl-navigation__link" href="">Link</a>--}}
+                    {{--<a class="mdl-navigation__link" href="">Link</a>--}}
+                {{--</nav>--}}
+            {{--</div>--}}
+            <main class="mdl-layout__content">
+                <div class="page-content">
+                    @yield('content')
+                </div>
+            </main>
         </div>
 
-        <script type="application/javascript" src="/mld/material.min.js"></script>
+        <script type="application/javascript" src="/mld/material.js"></script>
+        @yield('scripts')
 
     </body>
 </html>
