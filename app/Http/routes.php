d25b64230a6230dfa5eac72c7f8ba056ae7731af
Walter Neto <walter@catskillet.com>
2015-11-03 20:15:27 -0300

    retirando rotas de cadastro de usuarios...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,7 +23,3 @@ Route::group(['middleware' => 'auth'], function() {
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
