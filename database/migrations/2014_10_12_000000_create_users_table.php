595f892828d910264aab454e4ca2c4a6f237a0ea
Walter Neto <walter@catskillet.com>
2015-11-16 00:29:19 -0300

    correcao no codigo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,27 +12,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create(/**
-         * @param Blueprint $table
-         */
-            'users', function (Blueprint $table) {
-            $table->increments('id');/*matricula*/
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('telephone', 15)->nullable();
-            $table->string('cellphone', 15)->nullable();;
+            $table->string('cellphone', 15)->nullable();
             $table->string('password', 60);
             $table->string('city', 30)->nullable();
-            $table->string('street', 60);
-            $table->string('cep');
-            $table->string('uf', 2);
-            $table->enum('status', array(, ,  ));
+            $table->string('street', 60)->nullable();
+            $table->string('cep')->nullable();
+            $table->string('uf', 2)->nullable();
+            $table->enum('status', array(1,2,3))->nullable();
             $table->rememberToken();
             $table->timestamps();
-
-          /*CONSTRAINT co_pk_cod_user PRIMARY KEY(cod_usuario),
-          *CONSTRAINT co_fk_cod_per FOREIGN KEY(id_permissao) REFERENCES tb_permissao(cod_permissao)*/
-
         });
     }
 
