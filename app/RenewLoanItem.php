2172f90aff1cc89c23c8995e463c13b864222c21
Walter Neto <walter@catskillet.com>
2015-11-18 14:13:32 -0300

    adicionando campos, validate e store no model

--- app/RenewLoanItem.php
+++ app/RenewLoanItem.php
@@ -3,8 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class RenewLoanItem extends Model
 {
-    //
+    //loan_item_id, return_prevision
+
+    protected $fillable = [
+        'loan_item_id',
+        'return_prevision'
+    ];
+
+    protected $rules = [
+        'loan_item_id'      => 'required|integer',
+        'return_prevision'  => 'required|datetime'
+    ];
+
+    protected $messages = [
+        //
+    ];
+
+    public function validate($data) {
+        return Validator::make($data, $this->rules, $this->messages);
+    }
+
+    public function store($data) {
+        $obj = new RenewLoanItem();
+        $obj->loan_item_id      = $data['loan_item_id'];
+        $obj->return_prevision  = $data['return_prevision'];
+        return $obj;
+    }
 }
